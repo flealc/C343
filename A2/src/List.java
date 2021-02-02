cc3521c59ee9048bfa91c440ff5339592b0ec529
Fidel Leal <fleal@Fidels-MacBook-Pro.local>
2021-02-01 19:53:29 -0600

    add solution to A2

--- A2/src/List.java
+++ A2/src/List.java
@@ -1,5 +1,8 @@
+import javax.swing.*;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class EmptyListE extends Exception {}
 
@@ -12,15 +15,16 @@ class EmptyListE extends Exception {}
  * See the test cases for examples
  */
 abstract class List<E> {
-    static List<Integer> countdown (int n) {
+    static List<Integer> countdown(int n) {
         if (n == 0)
             return new NodeL<>(0, new EmptyL<>());
         else
-            return new NodeL<>(n, countdown(n-1));
+            return new NodeL<>(n, countdown(n - 1));
     }
+
     /**
      * Getter Methods
-     * */
+     */
     abstract E getFirst() throws EmptyListE;
 
     abstract List<E> getRest() throws EmptyListE;
@@ -39,61 +43,65 @@ abstract class List<E> {
     /**
      * Inserts newElem after every occurrence of elem
      */
-    abstract List<E> insertAfter (E elem, E newElem);
+    abstract List<E> insertAfter(E elem, E newElem);
 
     /**
      * Removes the first occurrence of elem (if
      * there is one)
      */
-    abstract List<E> removeFirst (E elem);
+    abstract List<E> removeFirst(E elem);
 
     /**
      * Returns the 0-based index of elem in the list
      * If the element is not in the list, throw
      * an exception
      */
-    abstract int indexOf (E elem) throws EmptyListE;
+    abstract int indexOf(E elem) throws EmptyListE;
 
     /**
      * Returns a new list that only contains the
      * elements satisfying the given predicate
      */
-    abstract List<E> filter (Function<E,Boolean> pred);
+    abstract List<E> filter(Function<E, Boolean> pred);
 
     /**
      * Returns a new list in which every element
      * is transformed using the given function
      */
-    abstract <F> List<F> map (Function<E,F> f);
+    abstract <F> List<F> map(Function<E, F> f);
 
     /**
      * Starting with base as the current result,
      * repeatedly applies the bifunction f to the
      * current result and one element from the list
      */
-    abstract <F> F reduce(F base, BiFunction<E,F,F> f);
+    abstract <F> F reduce(F base, BiFunction<E, F, F> f);
 
     /**
      * Appends the given list at the end of the
      * current list
      */
-    abstract List<E> append (List<E> other);
+    abstract List<E> append(List<E> other);
 
     /**
      * Use to accumulate the reverse of the given list
      */
-    abstract List<E> reverseHelper (List<E> result);
+    abstract List<E> reverseHelper(List<E> result);
 
     /**
      * Returns a big list containing all the sublists of
      * the current list
      */
-    abstract List<List<E>> powerSet ();
+    abstract List<List<E>> powerSet();
 
-    List<E> reverse () {
+    List<E> reverse() {
         return reverseHelper(new EmptyL<>());
     }
+
+    abstract boolean isEmpty();
+
 }
+
 
 //-------------------------------------------------------------
 
@@ -108,52 +116,57 @@ class EmptyL<E> extends List<E> {
     }
 
     int length() {
-        return 0; // TODO
+        return 0;                                           // DONE
     }
 
-    boolean inList(E elem) {
-        return false; // TODO
+    boolean inList(E elem)  {
+
+        return false;                                       // DONE
     }
 
-    List<E> insertAfter(E elem, E newElem) {
-        return null; // TODO
+    List<E> insertAfter(E elem, E newElem) {                // DONE
+        return new EmptyL<>();
     }
 
-    List<E> removeFirst(E elem) {
-        return null; // TODO
+    List<E> removeFirst(E elem) {                           // DONE
+        return new EmptyL<>();
     }
 
-    int indexOf(E elem) throws EmptyListE {
-        return 0; // TODO
+    int indexOf(E elem) throws EmptyListE {                 // DONE
+        throw new EmptyListE();
     }
 
-    List<E> filter(Function<E, Boolean> pred) {
-        return null; // TODO
+    List<E> filter(Function<E, Boolean> pred) {             // DONE
+        return new EmptyL<>();
     }
 
-    <F> List<F> map(Function<E, F> f) {
-        return null; // TODO
+    <F> List<F> map(Function<E, F> f) {                     // DONE
+        return new EmptyL<>();
     }
 
-    <F> F reduce(F base, BiFunction<E, F, F> f) {
-        return null; // TODO
+    <F> F reduce(F base, BiFunction<E, F, F> f) {           // DONE
+        return base;
     }
 
-    List<E> append(List<E> other) {
-        return null; // TODO
+    List<E> append(List<E> other) {                         // DONE
+        return other;
+
     }
 
-    List<E> reverseHelper(List<E> result) {
-        return null; // TODO
+    List<E> reverseHelper(List<E> result) {                 // DONE
+
+      return result;
     }
 
-    List<List<E>> powerSet() {
-        return null; // TODO
+    List<List<E>> powerSet() {                              // DONE
+        return new NodeL<List<E>>(new EmptyL<>(), new EmptyL<>());
     }
 
     public boolean equals (Object o) {
         return o instanceof EmptyL;
     }
+
+    boolean isEmpty() {return true;}
 }
 
 //-------------------------------------------------------------
@@ -175,49 +188,82 @@ class NodeL<E> extends List<E> {
       return this.rest;
     }
 
-    int length() {
-        return 0; // TODO
+    int length() {                                          // DONE
+        if (rest.isEmpty()) {
+            return 1;
+        } else {
+            return (1 + rest.length());
+        }
     }
 
-    boolean inList(E elem) {
-        return false; // TODO
+    boolean inList(E elem) {                                // DONE
+        if (first.equals(elem)) {
+            return true;
+        } else {
+            return rest.inList(elem);
+        }
     }
 
-    List<E> insertAfter(E elem, E newElem) {
-        return null; // TODO
+    List<E> insertAfter(E elem, E newElem) {                // DONE
+       if (first.equals(elem)) {
+           return (new NodeL<E>(first, new NodeL<>(newElem, rest.insertAfter(elem, newElem))));
+       } else {
+           return (new NodeL<E>(first, rest.insertAfter(elem, newElem)));
+       }
     }
 
     List<E> removeFirst(E elem) {
-        return null; // TODO
+        if (first.equals(elem)) {                           // DONE
+            return rest;
+        } else {
+            return (new NodeL<E>(first, rest.removeFirst(elem)));
+        }
     }
 
-    int indexOf(E elem) throws EmptyListE {
-        return 0; // TODO
+    int indexOf(E elem) throws EmptyListE {                 // DONE
+        if (first.equals(elem)){
+            return 0;
+        } else {
+            return (1 + rest.indexOf(elem));
+        }
     }
 
-    List<E> filter(Function<E, Boolean> pred) {
-        return null; // TODO
+    List<E> filter(Function<E, Boolean> pred) {             // DONE
+        if (pred.apply(first)) {
+            return new NodeL<E>(first, rest.filter(pred));
+        } else {
+            return rest.filter(pred);
+        }
     }
 
-    <F> List<F> map(Function<E, F> f) {
-        return null; // TODO
+    <F> List<F> map(Function<E, F> f) {                     // DONE
+        return new NodeL<>(f.apply(first), rest.map(f));
     }
 
-    <F> F reduce(F base, BiFunction<E, F, F> f) {
-        return null; // TODO
+
+    <F> F reduce(F base, BiFunction<E, F, F> f) {           // DONE
+        return f.apply(first, rest.reduce(base, f));
     }
 
-    List<E> append(List<E> other) {
-        return null; // TODO
+    List<E> append(List<E> other) {                         // DONE
+        return new NodeL(first, rest.append(other));
     }
 
-    List<E> reverseHelper(List<E> result) {
-        return null; // TODO
+
+
+    List<E> reverseHelper(List<E> result) {                 // DONE
+        if (rest.isEmpty())
+            return (new NodeL<>(first, result));
+        else return rest.reverseHelper(new NodeL<>(first, result));
     }
 
-    List<List<E>> powerSet() {
-        return null; // TODO
+    List<List<E>> powerSet() {                              // TODO
+        // return rest.powerSet().map(n -> new NodeL<NodeL<E>>(first, n).append(new NodeL<NodeL<E>>(rest.powerSet(), new EmptyL<>())));
+        // new NodeL<List<E>>()),rest.powerSet());
+
+   /**List<List<E>> whole= */ return rest.powerSet().append(rest.powerSet().map(n -> new NodeL<>(first, n)));
     }
+
 
     public boolean equals (Object o) {
         if (o instanceof NodeL) {
@@ -226,6 +272,8 @@ class NodeL<E> extends List<E> {
         }
         else return false;
     }
+
+    boolean isEmpty() {return false;}
 }
 
 //-------------------------------------------------------------
--- A2/test/ListTest.java
+++ A2/test/ListTest.java
@@ -44,8 +44,8 @@ class ListTest {
                                 new NodeL<>(1,
                                         new EmptyL<>())));
 
-        assertEquals(intsEvens, ints.filter(n -> n%2==0));
-        assertEquals(intsOdds, ints.filter(n -> n%2==1));
+        assertEquals(intsEvens, ints.filter(n -> n % 2 == 0));
+        assertEquals(intsOdds, ints.filter(n -> n % 2 == 1));
 
         List<Integer> intsSquared =
                 new NodeL<>(25,
@@ -56,7 +56,7 @@ class ListTest {
                                                         new NodeL<>(0,
                                                                 new EmptyL<>()))))));
 
-        assertEquals(intsSquared, ints.map(n -> n*n));
+        assertEquals(intsSquared, ints.map(n -> n * n));
 
         assertEquals(15, ints.reduce(0, Integer::sum));
 
@@ -94,4 +94,256 @@ class ListTest {
         assertTrue(evensPS.inList(new NodeL<>(2, new NodeL<>(0, new EmptyL<>()))));
         assertTrue(evensPS.inList(new NodeL<>(4, new NodeL<>(2, new NodeL<>(0, new EmptyL<>())))));
     }
+
+    /**
+     * My Test Cases
+     */
+
+    @Test
+    void length() {
+        List<Integer> emptyList = new EmptyL<>();
+        List<Integer> listOf1 = List.countdown(0);
+        List<Integer> listOf10 = List.countdown(9);
+
+        assertEquals(1, listOf1.length());
+        assertEquals(10, listOf10.length());
+        assertEquals(0, emptyList.length());
+
+    }
+
+    @Test
+    void inList() {
+        List<Integer> listOf10 = List.countdown(9);
+        List<Integer> emptyList = new EmptyL<>();
+
+        assertTrue(listOf10.inList(3));
+        assertTrue(listOf10.inList(9));
+        assertTrue(listOf10.inList(0));
+        assertFalse(listOf10.inList(10));
+        assertFalse(emptyList.inList(3));
+    }
+
+    @Test
+    void insertAfter() {
+        List<Integer> emptyList = new EmptyL<>();
+
+        List<Integer> missing3 =
+                new NodeL<>(5,
+                        new NodeL<>(4,
+                                new NodeL<>(2,
+                                        new NodeL<>(1,
+                                                new NodeL<>(0,
+                                                        new EmptyL<>())))));
+        List<Integer> double4 =
+                new NodeL<>(5,
+                        new NodeL<>(4,
+                                new NodeL<>(4,
+                                        new NodeL<>(1,
+                                                new NodeL<>(0,
+                                                        new EmptyL<>())))));
+
+        List<Integer> double43 = double4.insertAfter(4, 3);
+
+        assertEquals(emptyList.insertAfter(4, 5), emptyList);
+
+        List<Integer> complete = missing3.insertAfter(4, 3);
+        assertTrue(complete.inList(5));
+        assertTrue(complete.inList(4));
+        assertTrue(complete.inList(3));
+        assertTrue(complete.inList(2));
+        assertTrue(complete.inList(1));
+        assertTrue(complete.inList(0));
+        assertTrue(double43.inList(3));
+        assertEquals(5, missing3.length());
+        assertEquals(6, complete.length());
+        assertEquals(7, double43.length());
+
+    }
+
+    @Test
+    void removeFirst() {
+        List<Integer> listOf5 = List.countdown(5);
+        List<Integer> double4 = listOf5.insertAfter(4, 4);
+        List<Integer> removeDup4 = double4.removeFirst(4);
+        List<Integer> missing5 = listOf5.removeFirst(5);
+        List<Integer> missing3 = listOf5.removeFirst(3);
+        List<Integer> missing0 = listOf5.removeFirst(0);
+        List emptyList = new EmptyL<>();
+
+        assertEquals(emptyList.removeFirst(3),new EmptyL<>());
+        assertFalse(missing3.inList(3));
+        assertEquals(5, missing3.length());
+        assertEquals(listOf5, removeDup4);
+        assertFalse(missing0.inList(0));
+        assertTrue(missing0.inList(1));
+        assertTrue(missing0.inList(2));
+        assertTrue(missing0.inList(3));
+        assertTrue(missing0.inList(4));
+        assertTrue(missing0.inList(5));
+        assertTrue(missing5.inList(4));
+        assertEquals(5, missing5.length());
+
+    }
+
+    @Test
+    void indexOf() throws EmptyListE {
+        List<Integer> listOf5 = List.countdown(5);
+        List emptyList = new EmptyL<>();
+        assertEquals(0, listOf5.indexOf(5));
+        assertEquals(1, listOf5.indexOf(4));
+        assertEquals(2, listOf5.indexOf(3));
+        assertEquals(4, listOf5.indexOf(1));
+        assertEquals(5, listOf5.indexOf(0));
+        assertThrows(EmptyListE.class, () -> {emptyList.indexOf(3); });
+    }
+
+    @Test
+    void filter() {
+        List<Integer> listOf5 = List.countdown(5);
+        List<Integer> lessThan4 = listOf5.filter(n -> n <= 3);
+        List<Integer> notMultipleOf5 = List.countdown(50).filter(n -> n % 5 != 0);
+        List emptyList = new EmptyL<>();
+
+        assertEquals(emptyList.filter(n -> n.equals(listOf5)), new EmptyL<>());
+        assertFalse(lessThan4.inList(4));
+        assertFalse(lessThan4.inList(5));
+        assertTrue(lessThan4.inList(3));
+        assertTrue(notMultipleOf5.inList(3));
+        assertTrue(notMultipleOf5.inList(27));
+        assertTrue(notMultipleOf5.inList(43));
+        assertFalse(notMultipleOf5.inList(50));
+        assertFalse(notMultipleOf5.inList(25));
+        assertFalse(notMultipleOf5.inList(0));
+    }
+
+    @Test
+    void map() {
+        List<Integer> listOf5 =
+                new NodeL<>(5,
+                        new NodeL<>(4,
+                                new NodeL<>(3,
+                                        new NodeL<>(2,
+                                                new NodeL<>(1,
+                                                        new NodeL<>(0,
+                                                                new EmptyL<>()))))));
+        List<Integer> list5To10 =
+                new NodeL<>(10,
+                        new NodeL<>(9,
+                                new NodeL<>(8,
+                                        new NodeL<>(7,
+                                                new NodeL<>(6,
+                                                        new NodeL<>(5,
+                                                                new EmptyL<>()))))));
+
+        List emptyList = new EmptyL<>();
+
+        assertEquals(emptyList.map(n -> new NodeL<>(n, new EmptyL<>())), new EmptyL<>());
+        assertEquals(listOf5, list5To10.map(n -> n - 5));
+        assertEquals(list5To10, listOf5.map(n -> n + 5));
+    }
+
+    @Test
+    void reduce() {
+        List<Integer> list5To0 =
+                new NodeL<>(5,
+                        new NodeL<>(4,
+                                new NodeL<>(3,
+                                        new NodeL<>(2,
+                                                new NodeL<>(1,
+                                                        new NodeL<>(0,
+                                                                new EmptyL<>()))))));
+        List<Integer> list0To5 =
+                new NodeL<>(0,
+                        new NodeL<>(1,
+                                new NodeL<>(2,
+                                        new NodeL<>(3,
+                                                new NodeL<>(4,
+                                                        new NodeL<>(5,
+                                                                new EmptyL<>()))))));
+        List<Integer> emptyList = new EmptyL<>();
+
+        List<Integer> listOf6 = List.countdown(6);
+
+        assertEquals(list5To0.reduce(0, Integer::sum), list0To5.reduce(0, Integer::sum));
+        assertNotEquals(list5To0.reduce(0, Integer::sum), listOf6.reduce(0, Integer::sum));
+        assertEquals(0, emptyList.reduce(0, Integer::sum));
+
+    }
+
+    @Test
+    void append() {
+        List<String> abc =
+                new NodeL<>("a",
+                        new NodeL<>("b",
+                                new NodeL<>("c",
+                                        new EmptyL<>())));
+        List<String> def =
+                new NodeL<>("d",
+                        new NodeL<>("e",
+                                new NodeL<>("f",
+                                        new EmptyL<>())));
+
+        List<String> abcdef =
+                new NodeL<>("a",
+                        new NodeL<>("b",
+                                new NodeL<>("c",
+                                        new NodeL<>("d",
+                                                new NodeL<>("e",
+                                                        new NodeL<>("f",
+                                                                new EmptyL<>()))))));
+
+        List emptyList = new EmptyL<>();
+
+        assertEquals(abcdef, abc.append(def));
+        assertEquals(abc, emptyList.append(abc));
+    }
+
+    @Test
+    void reverseHelper() throws EmptyListE {
+        List<String> abc =
+                new NodeL<>("a",
+                        new NodeL<>("b",
+                                new NodeL<>("c",
+                                        new EmptyL<>())));
+        List<String> cba =
+                new NodeL<>("c",
+                        new NodeL<>("b",
+                                new NodeL<>("a",
+                                        new EmptyL<>())));
+        List<String> reverseCba = cba.reverse();
+        List emptyList = new EmptyL<>();
+
+        assertEquals(abc, cba.reverse());
+        assertEquals(0, reverseCba.indexOf("a"));
+        assertEquals(emptyList, emptyList.reverse());
+    }
+
+    @Test
+    void powerSet() {
+
+        List<String> abcdef =
+                new NodeL<>("a",
+                        new NodeL<>("b",
+                                new NodeL<>("c",
+                                        new NodeL<>("d",
+                                                new NodeL<>("e",
+                                                        new NodeL<>("f",
+                                                                new EmptyL<>()))))));
+        List<String> abc =
+                new NodeL<>("a",
+                        new NodeL<>("b",
+                                new NodeL<>("c",
+                                        new EmptyL<>())));
+        List emptyList = new EmptyL<>();
+
+        List<List<String>> abcdefPS = abcdef.powerSet();
+        assertTrue(abcdefPS.inList(abc));
+        assertTrue(abcdefPS.inList(new EmptyL<>()));
+        assertTrue(emptyList.powerSet().inList(new EmptyL<>()));
+    }
+
 }
+
+
+
+
