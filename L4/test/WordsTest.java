e02c9b8702443c06cfa61a8e8e14567ae90f5595
Annie Pompa <anniepompa@pop-os.localdomain>
2021-02-11 15:36:23 -0500

    starter code for L4

---
+++ L4/test/WordsTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+
+class WordsTest {
+
+    // write your own tests timing how long it takes to build a hashset with a Listof String and then a Listof Word
+    // refer to Lab 3 tests if you do not remember how to time tests using methods of the Instant & Duration class
+    @Test
+    void timeW () {
+        // TODO
+    }
+
+}
