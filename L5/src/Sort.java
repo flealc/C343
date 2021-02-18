280d6a7d9dfcef83105e4338978410ac6a626c32
Annie Pompa <anniepompa@pop-os.localdomain>
2021-02-18 15:31:38 -0500

    lab5 starter code

---
+++ L5/src/Sort.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sort {
+
+    // This method should sort the input list using the algorithm for insertion sort.
+    // That is, first create a new ArrayList with all of the elements from input ns.
+    // Then, iterate through this new ArrayList - comparing a current element to its
+    // predecessor. While current is less, it is swapped w predecessor.
+
+    // For those who prefer wordier instructions, check out Lab 5 post on canvas :)
+    // Otherwise, best of luck on the lab! Tests/debugging will help a lot with IndexOutOfBoundsExceptions
+    static List<Integer> insertionSort (List<Integer> ns) {
+        // TODO
+        return null;
+    }
+}
---
+++ L5/test/SortTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+
+class SortTest {
+
+    @Test
+    void sort () {
+        // TODO more correctness tests
+        List<Integer> ns = Arrays.asList(70,100,203,784,412,135,101);
+
+        System.out.printf("Original list = %s%n", ns);
+        System.out.printf("insertionSort = %s%n%n", Sort.insertionSort(ns));
+    }
+
+    void timeSort () {
+        Instant start = Instant.now();
+        // TODO efficiency tests
+        // In particular, include a test that demonstrates why insertion sort's worst-case
+        // performance is O(n^2)
+        Instant end = Instant.now();
+        Duration time = Duration.between(start, end);
+        System.out.println(time.toMillis());
+    }
+
+}
