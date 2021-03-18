61a8c801cf43e31e31f4fac1ffd659d47a7765db
Annie Pompa <apompa@iu.edu>
2021-03-18 14:56:43 -0400

    starter code for L9

---
+++ L9/test/TreeTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TreeTest {
+
+    // TODO does it work ?
+    @Test
+    public void sort () {
+    }
+}
